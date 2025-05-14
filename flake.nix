d65ddabdb81d3ce4fdb7caec663e3115cf3a01fa
André Lima <me@limwa.pt>
2025-05-14 14:38:43 +0100

    chore: add nix tooling

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A flake for MongoDB Kubernetes development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        formatter = pkgs.alejandra;
+
+        devShell = import ./shell.nix {
+          inherit pkgs;
+        };
+      }
+    );
+}
